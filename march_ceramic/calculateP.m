cb51d53efb9ff17f873956731905a403a28e5882
Anna Barth <anna2nsb@gmail.com>
2024-04-05 16:05:06 -0400

    idk anymore idk

--- march_ceramic/calculateP.m
+++ march_ceramic/calculateP.m
@@ -4,8 +4,8 @@ eta_0V = dataTable(dataTable(:,1)==phi & dataTable(:,2)==sigma & dataTable(:,3)=
 gamma_dot_0V = sigma/eta_0V;
 
 %P = V^2/sigma/gamma_dot_0V;
-%P = V^2/sigma/gamma_dot_0V*Q_factor(phi,sigma);
-P = V^2 * (0.678-phi)^3.83 / gamma_dot_0V;
+P = V^2/sigma/gamma_dot_0V*Q_factor(phi,sigma);
+%P = V^2 * (0.678-phi)^3.83 / gamma_dot_0V;
 return
 %P = V^2/sigma/gamma_dot_0V*exp(-1/(0.68-phi));
 %P = V^2/sigma/gamma_dot_0V*exp(-(1/(0.68-phi))^0.75);
