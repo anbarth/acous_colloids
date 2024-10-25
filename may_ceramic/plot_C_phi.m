ff04249fdcaf2af6297e427d07a9f7d2e43bb71e
Anna Barth <anna2nsb@gmail.com>
2024-10-25 17:17:03 -0400

    fjkldfjklsd

--- may_ceramic/plot_C_phi.m
+++ may_ceramic/plot_C_phi.m
@@ -1,6 +1,6 @@
 function plot_C_phi(stressTable, paramsVector)
 
-alpha = 1;
+alpha = 0;
 
 [eta0, phi0, delta, A, width, sigmastar, C, phi_fudge] = unzipParams(paramsVector,13);
 phi_list = unique(stressTable(:,1));
@@ -10,8 +10,8 @@ volt_list = [0,5,10,20,40,60,80];
 hold on;
 
 
-xlim([0.15 0.75])
-ylabel('D')
+%xlim([0.15 0.75])
+ylabel(strcat('C_',num2str(alpha)))
 xlabel('\phi')
 %xline(phi0)
 
@@ -25,10 +25,16 @@ myC = myC(myC~=0);
 
 myC = myC .* (phi0-myPhi).^alpha;
 
-%ax1=gca; ax1.XScale='log'; ax1.YScale='log';
+ax1=gca; ax1.XScale='log'; ax1.YScale='log';
 %plot(phi0-myPhi,myC,'-o','LineWidth',1.5);
-plot(myPhi,myC,'-o','LineWidth',1.5);
-title(alpha)
+%plot(phi0-myPhi,1./myC,'-o','LineWidth',1.5);
+plot(phi0-myPhi,log(myC),'-o','LineWidth',1.5);
 
+B = -50;
+a = 3;
+log_D_fake = B*(phi0-myPhi).^a;
+plot(phi0-myPhi,log_D_fake,'-','LineWidth',1);
+
+%ylim([0 6])
 
 end
