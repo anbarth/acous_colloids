62815335a4dbd371591a23f4d7758e3abbc7091e
Anna Barth <anna2nsb@gmail.com>
2025-02-21 17:37:59 -0500

    fjkljfd

---
+++ may_ceramic_2/autocollapse/modelHandpickedAllExp_CSV.m
@@ -0,0 +1,15 @@
+function [x,F,delta_F,F_hat,eta,delta_eta,eta_hat] = modelHandpickedAllExp_CSV(stressTable, y)
+
+CSV=25;
+
+[x,F,delta_F,F_hat,eta,delta_eta,eta_hat] = modelHandpickedAllExp(stressTable, y);
+
+F=F*CSV;
+delta_F=delta_F*CSV;
+F_hat=F_hat*CSV;
+eta=eta*CSV;
+delta_eta=delta_eta*CSV;
+eta_hat=eta_hat*CSV;
+
+
+end
---
+++ may_ceramic_2/hand_collapse_02_11/make_fig_3.m
@@ -0,0 +1,27 @@
+load("optimized_params_02_11.mat")
+y = y_fminsearch;
+
+myModelHandle = @modelHandpickedAllExp_CSV;
+
+errBars = false;
+show_F_vs_x(dataTable,y,myModelHandle,'ShowErrorBars',errBars)
+prettyPlot;
+xlim([1e-22 10])
+ylim([0.2 200])
+show_F_vs_xc_x(dataTable,y,myModelHandle,'ShowErrorBars',errBars)
+prettyPlot;
+ylim([0.2 200])
+
+% compare with uncollapsed data?
+show_F_vs_x(dataTable,y_lsq_0V,@modelHandpickedAllExp0V_CSV,'ShowErrorBars',errBars)
+prettyPlot;
+xlim([1e-22 10])
+ylim([0.2 200])
+show_F_vs_xc_x(dataTable,y_lsq_0V,@modelHandpickedAllExp0V_CSV,'ShowErrorBars',errBars)
+prettyPlot;
+ylim([0.2 200])
+
+% show_F_vs_x(dataTable,y_lsq_0V,@modelHandpickedAllExp0V_CSV,'ShowErrorBars',true,'ShowLines',true,'PhiRange',10)
+% xlim([0.1 1.5])
+% ylim([0.3 30])
+% prettyPlot;
