aee65df6e8f527f77c4ff7c5c4e0b2a6a662e4dd
Anna Barth <anna2nsb@gmail.com>
2025-02-25 09:42:43 -0500

    fd

--- may_ceramic_2/ness_data/ness_collapse_02_22.m
+++ may_ceramic_2/ness_data/ness_collapse_02_22.m
@@ -11,14 +11,27 @@ numV = length(volt_list);
 phi0 = 0.6482; % from ness_find_phi0_exclude_lower_phi
 
 f = @(sigma,sigmastar) exp(-sigmastar./sigma);
-[eta0,sigmastar,phimu] = ness_wyart_cates_fix_phi0(dataBelowSJ,f,phi0,true);
+[eta0,sigmastar,phimu] = ness_wyart_cates_fix_phi0(dataBelowSJ,f,phi0,false);
 %return
 
-C = ones(1,length(phi_list));
-
+%C = ones(1,length(phi_list));
 D = (phi0-phimu)./(phi0-phi_list)';
-D = D.*C;
-D = D/1.2; % to ensure all data fall below x=1
+Dboring = D;
+for ii=1:length(phi_list)
+    phi = phi_list(ii);
+    if phi < 0.59
+        continue
+    end
+    stress = dataTable(dataTable(:,1)==phi,2);
+    sigmaJ = max(stress);
+    D(ii) = 1/f(sigmaJ,sigmastar);
+end
+
+
+
+
+%D = D.*C;
+%D = D/1.2; % to ensure all data fall below x=1
 
 %[eta0, phi0, delta, A, width, sigmastar, D]
 y_init = [eta0,phi0,-3,eta0,0.5,sigmastar,D];
@@ -26,3 +39,16 @@ myModelHandle = @modelNessExp;
 
 show_F_vs_x(dataTable,y_init,myModelHandle,'ShowLines',true,'ColorBy',2,'PhiRange',10:-1:1)
 xlim([1e-4 2])
+
+figure; hold on; ax1=gca; ax1.XScale='log'; ax1.YScale='log';
+% D vs phi0-phi
+dphi = phi0-phi_list;
+plot(dphi, D,'-o');
+p = polyfit(log(dphi), log(D),1);
+%plot(dphi,exp(polyval(p,log(dphi))),'-r');
+plot(dphi,Dboring,'r-');
+
+figure; hold on; 
+% D vs phi
+plot(phi_list, D,'-o');
+plot(phi_list,Dboring,'r-')
