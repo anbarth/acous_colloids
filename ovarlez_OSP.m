ef08f9392b4ca3d2899239ed50de3f015919bf12
anbarth <anna2nsb@gmail.com>
2025-06-12 13:06:30 -0400

    fjkfsd

--- ovarlez_OSP.m
+++ ovarlez_OSP.m
@@ -10,13 +10,27 @@ ovarlez_data = [0.708502024291497, 0.1265822784810129
 12.004048582995953, 1.708860759493671
 15.20242914979757, 2.0886075949367093];
 
-f = [10 50 15 20 50 25 30 50 35 40 50 45];
-A = [500 100 500 500 200 500 500 300 500 500 400 500];
+f = [10 50 15 20 50 25 30 50 35 40 50 45]; % Hz
+A = [500 100 500 500 200 500 500 300 500 500 400 500]*1e-6; % m 
+% delete one point to be consistent with fig 7
+f(7)=[];
+A(7)=[];
 
-crit_rate = ovarlez_data(:,1);
-sigma_v = ovarlez_data(:,2);
+%rho_solvent = 1000;
+%rho_particle = 1300;
+%phi_w = .29;
+%rho_s = phi_w*rho_particle+(1-phi_w)*rho_solvent;
 
-rate_OSP = 2*pi*f .* A*10^-3/28;
+% checking correspondance bw fig 6 and fig 7 data points
+rho_s = 1521.3;
+sigma_v_calculated = 1/2*rho_s * A.^2 .* (2*pi*f).^2;
+
+
+rate_c = ovarlez_data(:,2);
+sigma_c = 10*rate_c;
+sigma_v = ovarlez_data(:,1);
+
+rate_OSP = 2*pi*f .* A/(28e-3);
 
 
 
@@ -25,10 +39,18 @@ rate_OSP = 2*pi*f .* A*10^-3/28;
 
 %return
 figure; hold on;
-xlabel('\sigma_v'); ylabel('rate_c')
-plot(sigma_v,crit_rate,'-o')
+xlabel('\sigma_v'); ylabel('\sigma_c')
+plot(sigma_v,sigma_c,'o')
+k_v = 1.5;
+x = linspace(min(sigma_v),max(sigma_v));
+plot(x,k_v*x,'b-')
 
-rate_OSP = (sigma_v*2/200).^(1/2)/(28*10^-3); % proportional to gamma_OSP but missing prefactor
+%rate_OSP = (sigma_v*2/200).^(1/2)/(28*10^-3); % proportional to gamma_OSP but missing prefactor
 figure; hold on;
 xlabel('rate_{OSP}'); ylabel('rate_c')
-plot(rate_OSP,crit_rate,'-o')
+plot(rate_OSP,rate_c,'o')
+plot(0,0,'ko','MarkerFaceColor','k')
+k_OSP = 0.5;
+%offset_OSP = -0.5;
+x = linspace(min(rate_OSP),max(rate_OSP));
+plot(x,k_OSP*x+offset_OSP,'b-')
