1ff7c58c5447fec34619d0213e41e50626dd052f
Anna Barth <anna2nsb@gmail.com>
2025-06-17 09:21:35 -0400

    fjkljfd

--- LLPS_thermal_expansion.m
+++ LLPS_thermal_expansion.m
@@ -1,5 +1,5 @@
 % some model for thermal expansion of the medium
-a = @(phi) 1;
+a = @(phi) 1+phi;
 b = @(phi) 1/1000;
 rho = @(phi,T) a(phi)+b(phi).*T;
 
@@ -16,8 +16,6 @@ phi1 = @(T) phistar - c_left*(Tstar-T).^(1/2);
 phi2 = @(T) phistar + c_right*(Tstar-T).^(1/2);
 
 % n1, n2 are particle number densities of phase 1 and phase 2
-%n1 = @(phi,phi1,phi2) phi1./phi .* (phi2-phi)./(phi2-phi1);
-%n2 = @(phi,phi1,phi2) phi2./phi .* (phi-phi1)./(phi2-phi1);
 u1 = @(phi1,phi2,rho1,rho2,m) (phi2*m-rho2)./(phi2.*rho1-phi1.*rho2);
 u2 = @(phi1,phi2,rho1,rho2,m) (rho1-phi1*m)./(phi2.*rho1-phi1.*rho2);
 rho1 = @(T) rho(phi1(T),T);
@@ -37,6 +35,8 @@ xlabel('\phi'); ylabel('T')
 T = linspace(0,Tstar);
 plot(phi1(T),T,'k');
 plot(phi2(T),T,'k');
+xlim([min(phi1(T)) max(phi2(T))])
+ylim([min(T) max(T)])
 
 % plot the line followed in the experiment
 phi_i = 0.58; % initial concentration
@@ -45,5 +45,25 @@ rho_i = rho(phi,T_exp(1)); % initial density
 m = rho_i/phi_i; % mass M/N
 
 % TODO need to make it kink at the boundary
-phi_exp = phi_LLPS(m,T_exp);
+% this equation is only OK bc rho doesn't actually have any
+% phi-dependence.... how does it work when there is phi-dependence
+%rho_without_LLPS = rho(phi_i,T_exp);
+rho_without_LLPS = (a(0)+b(0)*T)/(m-1)*m;
+phi_without_LLPS = rho_without_LLPS/m;
+phi_exp = zeros(size(T_exp));
+for i = 1:length(T_exp)
+    if phi_without_LLPS(i) > phi1(T_exp(i)) && phi_without_LLPS(i) < phi2(T_exp(i))
+        phi_exp(i) = phi_LLPS(m,T_exp(i));
+    else
+        phi_exp(i) = phi_without_LLPS(i);
+    end
+end
+plot(phi_without_LLPS,T_exp,'r-')
 plot(phi_exp,T_exp,'k-')
+
+rho_exp = m*phi_exp;
+figure; hold on; xlabel('T'); ylabel('\rho')
+plot(T_exp,m*phi_without_LLPS,'r-')
+plot(T_exp,rho_exp,'k-')
+%plot(T_exp,rho1(T_exp),'b-')
+%plot(T_exp,rho2(T_exp),'g-')
