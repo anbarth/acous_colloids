ab14d3c70eec3ce9c3846f7e48d18734a9912fdd
Anna Barth <anna2nsb@gmail.com>
2025-08-05 15:08:10 -0400

    fjkjhdflhjksd

---
+++ may_ceramic_2/display_viscosity_data/stress_vs_acous_energy_with_predictions_03_19.m
@@ -0,0 +1,93 @@
+%optimize_sigmastarV_03_19;
+y = y_fmincon; myModelHandle = @modelHandpickedSigmastarV;
+
+dataTable = may_ceramic_09_17;
+my_vol_frac_markers = ["o","o","o","o","o","square","<","hexagram","^","pentagram","v","d",">"];
+CSS = (50/19)^3;
+
+
+phi_list = unique(dataTable(:,1));
+for phiNum = 10
+%for phiNum=6:13
+    
+    phi = phi_list(phiNum);
+    markerCode = strcat('-',my_vol_frac_markers(phiNum));
+    
+    
+    fig_eta = figure;
+    ax_eta = axes('Parent', fig_eta);
+    ax_eta.XLabel.String = 'Acoustic energy density{\it U_a} (Pa)';
+    ax_eta.YLabel.String = 'Shear stress \sigma (Pa)';
+    ax_eta.Title.String = strcat("\phi=",num2str(phi));
+    hold(ax_eta,'on');
+    cmap = winter(256);
+    colormap(ax_eta,cmap);
+    
+
+    %myData = dataTable(dataTable(:,1)==phi , :);
+    myData = dataTable(dataTable(:,1)==phi & dataTable(:,3)>0, :);
+    sigma_list = CSS*myData(:,2);
+    eta_list = CSS*myData(:,4);
+    rate_list = sigma_list./eta_list;
+    minLogRate = log(min(rate_list));
+    maxLogRate = log(max(rate_list));
+    
+
+    for ii=1:size(myData,1)
+    
+        phi = myData(ii,1);
+        sigma = CSS*myData(ii,2);
+        V = myData(ii,3);
+        eta = CSS*myData(ii,4);
+        rate = sigma/eta;
+    
+        myColor = cmap(round(1+255*(log(rate)-minLogRate)/(maxLogRate-minLogRate)),:);
+    
+        deltaEta = max(CSS*myData(ii,5),eta*0.15);
+        delta_phi = 0.02;
+        delta_eta_volumefraction = eta*2*(0.7-phi)^(-1)*delta_phi;
+        delta_eta_total = sqrt(deltaEta.^2+delta_eta_volumefraction.^2);
+    
+        delta_rate = rate * delta_eta_total/eta;
+        
+    
+        plot(ax_eta,acoustic_energy_density(V),sigma, markerCode,'Color',myColor,'MarkerFaceColor',myColor,'LineWidth',1);
+    
+    end
+    
+    for rate = logspace(log10(min(rate_list)),log10(max(rate_list)),5)
+        myColor = cmap(round(1+255*(log(rate)-minLogRate)/(maxLogRate-minLogRate)),:);
+        v_list = [0 5 10 20 40 60 80];
+        sigma_list = zeros(size(v_list));
+        for kk = 1:length(v_list)
+            v = v_list(kk);
+            solveMe =  @(logsigma) exp(logsigma)/viscosity_prediction(phi,exp(logsigma),v,dataTable,y,myModelHandle)-rate;
+            sigma_list(kk) = exp(fzero(solveMe,1.5));
+        end
+        plot(acoustic_energy_density(v_list),sigma_list*CSS,'Color',myColor)
+    end
+    
+    
+    c1 = colorbar(ax_eta);
+    clim(ax_eta,[minLogRate maxLogRate]);
+    %legend(L)
+    
+    
+    
+    
+    prettyPlot;
+    myfig = gcf;
+    myfig.Position=[457,388,414,343];
+    
+    ax1=gca;
+    %ax1.XScale = 'log';
+    %ax1.YScale = 'log';
+    
+    %yticks([10 100]);
+    %xlim([0.05 50])
+    %xticks([10^-1 10^0 10^1])
+    %ylim([9 165])
+    
+    %title(phi)
+
+end
--- may_ceramic_2/hand_collapse_sigmastarV_03_19/make_fig_3_03_19.m
+++ may_ceramic_2/hand_collapse_sigmastarV_03_19/make_fig_3_03_19.m
@@ -6,7 +6,11 @@ dataTable = may_ceramic_09_17;
 
 show_F_vs_x(dataTable,y,myModelHandle); ylim([0.3 1e2]); xlim([1e-20 10]); prettyPlot;
 show_F_vs_x(dataTable,y,myModelHandle); ylim([0.3 1e2]); xlim([1e-2 1.5]); prettyPlot;
-show_F_vs_xc_x(dataTable,y,myModelHandle); ylim([0.3 1e2]); prettyPlot;
+show_F_vs_xc_x(dataTable,y,myModelHandle,'ColorBy',3,'ShowInterpolatingFunction',true); prettyPlot;
+ylim([1e-1 2e2])
+xlim([7e-4 1])
+yticks([1e-1 1e2])
+xticks([1e-3 1e-2 1e-1 1])
 
 sigmastar = y(6:12);
 sigmastar_noV = sigmastar(1)*ones(size(sigmastar));
@@ -15,4 +19,4 @@ y_noV(6:12) = sigmastar_noV;
 
 show_F_vs_x(dataTable,y_noV,myModelHandle); ylim([0.3 1e2]); xlim([1e-20 10]); prettyPlot;
 show_F_vs_x(dataTable,y_noV,myModelHandle); ylim([0.3 1e2]); xlim([1e-2 1.5]); prettyPlot;
-show_F_vs_xc_x(dataTable,y_noV,myModelHandle); ylim([0.3 1e2]); prettyPlot;
+show_F_vs_xc_x(dataTable,y_noV,myModelHandle,'ColorBy',3); ylim([0.3 1e2]); prettyPlot;
